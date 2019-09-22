ea9e297e333f82398c290bec41bd1003e2542fea
AIRAT1 <ayrat1@mail.ru>
2019-09-22 12:12:46 +0300

    The Product class was created and implemented adding and display arbitrary amount of products in Main class.

---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    private static List<Product> products;
+    public static void main(String[] args) {
+        initList();
+        initProducts();
+        showProducts(products);
+    }
+
+    private static void initList() {
+        products = new ArrayList<>();
+    }
+
+    private static void initProducts() {
+        Product longer = new Product("https://s82079.cdn.ngenix.net/sRrSzWpf86c4hMMwCJ7aHBph.png?dw=230",
+                50,
+                "Лонгер BBQ",
+                107,
+                "Сочная курочка, маринованные огурчики, лук и аппетитный соус барбекю! Спешите попробовать!",
+                236,
+                9.7,
+                7.1,
+                33.3);
+        Product cheeseburger = new Product("https://s82079.cdn.ngenix.net/9VW3RQfY33AnJi7Yu38nKiHc.png?dw=230",
+                69,
+                "Чизбургер",
+                167,
+                "Пряный горчичный соус, кетчуп, сочные стрипсы в оригинальной панировке, лук, сыр Чеддер, огурцы на пшеничной булочке с кукурузной посыпкой.",
+                237,
+                13.2,
+                8.6,
+                26.6);
+        addProductsToTheList(longer, cheeseburger);
+    }
+
+    private static void addProductsToTheList(Product ... product) {
+        Collections.addAll(products, product);
+    }
+
+    private static void showProducts(List products) {
+        if (!products.isEmpty()) {
+            for (Object product : products) {
+                System.out.println(product);
+            }
+        }else {
+            System.out.println("Add products to the list");
+        }
+    }
+}
---
+++ src/main/java/Product.java
@@ -0,0 +1,38 @@
+public class Product {
+    private String productPictureUrl;
+    private int price;
+    private String name;
+    private int weight;
+    private String description;
+    private int caloriesQuantity;
+    private double proteinQuantity;
+    private double fatQuantity;
+    private double carbohydratesQuantity;
+
+    Product(String productPictureUrl, int price, String name, int weight, String description, int caloriesQuantity, double proteinQuantity, double fatQuantity, double carbohydratesQuantity) {
+        this.productPictureUrl = productPictureUrl;
+        this.price = price;
+        this.name = name;
+        this.weight = weight;
+        this.description = description;
+        this.caloriesQuantity = caloriesQuantity;
+        this.proteinQuantity = proteinQuantity;
+        this.fatQuantity = fatQuantity;
+        this.carbohydratesQuantity = carbohydratesQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return name +
+                " " +
+                "{productPictureUrl='" + productPictureUrl + '\'' +
+                ", price=" + price +
+                ", weight=" + weight +
+                ", description='" + description + '\'' +
+                ", caloriesQuantity=" + caloriesQuantity +
+                ", proteinQuantity=" + proteinQuantity +
+                ", fatQuantity=" + fatQuantity +
+                ", carbohydratesQuantity=" + carbohydratesQuantity +
+                '}';
+    }
+}
